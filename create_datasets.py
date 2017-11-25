fe4138dbb28b23af308d15da8ee4f55e20523852
Ismaïl Senhaji <ismail.senhaji@gmail.com>
2017-11-25 10:47:46 +0100

    Cleanup create_datasets

--- create_datasets.py
+++ create_datasets.py
@@ -1,65 +1,27 @@
-
-# coding: utf-8
-
-# In[255]:
-
-
-import os
 import pandas as pd
-import numpy as np 
 
+N_CLASSES = 5
 
-# In[256]:
-
-
-n_classes = 5
-
-
-# In[257]:
-
-
-# Get and clean occupancy data
-
-od=pd.read_csv("data.json", parse_dates=[0])
+# read occupancy data
+od = pd.read_csv("data.json", parse_dates=[0])
 
 od = od[od.capacity > 0]
-od["occupancy_percent"] = od.occupancy / od.capacity
+od.occupancy = od.occupancy / od.capacity
 
-od["week_day"] = od.time.dt.weekday
-od["hour"] = od.time.dt.hour
+od['week_day'] = od.time.dt.weekday
+od['hour'] = od.time.dt.hour
 
-del_columns = ['occupancy', 'capacity']
-od.drop(del_columns, 1, inplace=True)
+od.drop('capacity', 1, inplace=True)
 
+# read and clean weather data
+wd = pd.read_json("weather.json")
+wd.time = pd.to_datetime(wd.time, unit='s')
+wd.interpolate(limit_direction='both', inplace=True)
 
-# In[258]:
-
-
-# read an clean weather data
-
-wd=pd.read_json("weather.json")
-wd['date'] = pd.to_datetime(wd['time'], unit='s')
-wd.interpolate(limit_direction="both", inplace=True)
-
-del_columns = ['icon', 'precipType', 'summary', 'time']
+del_columns = ['icon', 'precipType', 'summary']
 wd.drop(del_columns, 1, inplace=True)
 
-
-# In[259]:
-
-
 # merge datasets
-
-mixed = od.merge(wd,how='left', left_on='time', right_on='date', left_index=True)
-mixed.drop('time', 1, inplace=True)
-mixed.reset_index(inplace=True)
-classes = mixed.occupancy_percent * n_classes
-classes = classes.round().astype('int')
-mixed.drop('occupancy_percent', 1, inplace=True)
-
-
-# In[260]:
-
-
-
-
+df = od.merge(wd, how='left', left_on='time', right_on='time', left_index=True)
+df.reset_index(inplace=True)
+df['class'] = (df.occupancy * N_CLASSES).round().astype('int')
