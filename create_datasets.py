252553e9d9122e165f91990d9c6470c86fddb3ba
polchky <polchky@gmail.com>
2017-11-25 11:59:16 +0100

    Remove NaN values

--- create_datasets.py
+++ create_datasets.py
@@ -7,7 +7,7 @@ od = pd.read_csv("data.json", parse_dates=[0])
 
 od = od[od.capacity > 0]
 od.occupancy = od.occupancy / od.capacity
-
+od.time.dt.round('1h', inplace=True)
 od['week_day'] = od.time.dt.weekday
 od['hour'] = od.time.dt.hour
 
@@ -24,5 +24,5 @@ wd.drop(del_columns, 1, inplace=True)
 # merge datasets
 df = od.merge(wd, how='left', left_on='time', right_on='time', left_index=True)
 df.reset_index(inplace=True)
-df.drop('index', 1, inplace=True)
 df['class'] = (df.occupancy * N_CLASSES).round().astype('int')
+df.dropna(inplace=True)
